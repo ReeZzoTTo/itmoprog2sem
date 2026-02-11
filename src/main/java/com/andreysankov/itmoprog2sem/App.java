fd54997c10d127b1d66524054a7eb499b4f90c18
Andrey Sankov <diomand386@gmail.com>
2026-02-11 18:36:43 +0300

    Создание Maven - стандартная архитектура

---
+++ src/main/java/com/andreysankov/itmoprog2sem/App.java
@@ -0,0 +1,13 @@
+package com.andreysankov.itmoprog2sem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
