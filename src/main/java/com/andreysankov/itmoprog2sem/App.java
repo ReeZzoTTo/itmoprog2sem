5d4a772739387d2dd7e71b96e145f9dcb6683ba6
Andrey Sankov <diomand386@gmail.com>
2026-02-16 16:14:07 +0300

    Добавлена команда group_counting_by_creation_date

--- src/main/java/com/andreysankov/itmoprog2sem/App.java
+++ src/main/java/com/andreysankov/itmoprog2sem/App.java
@@ -45,6 +45,7 @@ public class App {
         commandManager.registerCommand("exit", new ExitCommand(context));
         commandManager.registerCommand("history", new HistoryCommand(context));
         commandManager.registerCommand("average_of_personal_qualities_maximum", new AverageOfPersonalQualititesMaximumCommand(context));
+        commandManager.registerCommand("group_counting_by_creation_date", new GroupCountingByCreationDateCommand(context));
         
         System.out.println("Программа для управления колекцией");
         System.out.println("Чтение файла : " + context.getFileManager().getFileName());
